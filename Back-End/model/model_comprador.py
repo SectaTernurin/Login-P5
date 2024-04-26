f30baea57ddf76223082f711e12d7bf87f5a15ee
LorenMatCan <saramatutec@ciencias.unam.mx>
2024-04-25 19:07:52 -0600

    Se añade funcionaidad básica react

--- Back-End/model/model_comprador.py
+++ Back-End/model/model_comprador.py
@@ -8,8 +8,8 @@ def get_compradores():
 
 def get_log_In(correo, contrasena):
     compradorValido = Comprador.query.filter_by(correo = correo).first()
+    print(compradorValido)
     if compradorValido != None:
         if compradorValido.contrasena != contrasena:
             compradorValido = None
-    print(compradorValido)
     return compradorValido
