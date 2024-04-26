ecb82d2ed02f5e2056939e4ad261195ab7734782
Regina Del Razo <124300070+regis1flarrow@users.noreply.github.com>
2024-04-26 02:48:34 -0600

    Update App.js
    cambio de libreria de cookies

--- ternutianguis/src/App.js
+++ ternutianguis/src/App.js
@@ -1,49 +1,53 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
-import { Login } from './paginas/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './paginas/Login'; // Asegúrate de importar el componente por defecto
 import { Home } from './paginas/Home';
 import { Inicio } from './paginas/Inicio';
-import { Container } from 'react-bootstrap';
-import { Nav } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { CookiesProvider } from 'react-cookie'; // Importar CookiesProvider
+import Cookies from 'js-cookie'; // Importamos Cookies de js-cookie
 
 function App() {
+  // Función para establecer una cookie del idioma preferido del usuario
+  const setLanguageCookie = (language) => {
+    Cookies.set('language', language, { expires: 7 }); // Cookie expira en 7 días
+  }
+
+  // Función para obtener la cookie del idioma
+  const getLanguageCookie = () => {
+    return Cookies.get('language'); // Obtiene la cookie 'language'
+  }
+
   const currentPage = window.location.pathname;
 
   return (
-    <CookiesProvider> {/* Envolver todo el componente App con CookiesProvider */}
-      <div className="App">
-        <Router>
-          <div>
-            <Navbar bg="light" variant="light">
-              <Container>
-                <Nav className="me-auto">
-                  {currentPage === '/' ? (
-                    <Nav.Link href="/login">Log in</Nav.Link>
-                  ) : currentPage === '/login' ? (
-                    <Nav.Link href="/">Regresar</Nav.Link>
-                  ) : (
-                    <Nav.Link href="/">Log out</Nav.Link>
-                  )}
-                </Nav>
-              </Container>
-            </Navbar>
+    <div className="App">
+      <Router>
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Nav className="me-auto">
+              {currentPage === '/' ? (
+                <Nav.Link href="/login">Log in</Nav.Link>
+              ) : currentPage === '/login' ? (
+                <Nav.Link href="/">Regresar</Nav.Link>
+              ) : (
+                <Nav.Link href="/">Log out</Nav.Link>
+              )}
+            </Nav>
+          </Container>
+        </Navbar>
 
-            <div className="container">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/inicio" element={<Inicio />} />
-                <Route path="*" element={<h1>404 Not Found</h1>} />
-              </Routes>
-            </div>
-          </div>
-        </Router>
-      </div>
-    </CookiesProvider>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/inicio" element={<Inicio />} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
