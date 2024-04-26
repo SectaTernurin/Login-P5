711e708bef0086309f544db96edc89f068213fba
Regina Del Razo <124300070+regis1flarrow@users.noreply.github.com>
2024-04-26 02:09:37 -0600

    Update App.js
    Ya ahora si con cookies :)

--- ternutianguis/src/App.js
+++ ternutianguis/src/App.js
@@ -1,46 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes,Route,Link,useLocation} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { Login } from './paginas/Login';
 import { Home } from './paginas/Home';
 import { Inicio } from './paginas/Inicio';
-import { Container}  from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CookiesProvider } from 'react-cookie'; // Importar CookiesProvider
 
 function App() {
   const currentPage = window.location.pathname;
 
   return (
-    <div className="App">
-      <Router>
-      <div>
-      <Navbar bg="light" variant="light">
-          <Container>
-            <Nav className="me-auto">
-            {currentPage === '/' ? (
-                  <Nav.Link href="/login">Log in</Nav.Link>
-              ) : currentPage === '/login' ? (
-                  <Nav.Link href="/">Regresar</Nav.Link>
-              ) : (
-                <Nav.Link href="/">Log out</Nav.Link>
-              )}
-            </Nav>
-            </Container>
-        </Navbar>
+    <CookiesProvider> {/* Envolver todo el componente App con CookiesProvider */}
+      <div className="App">
+        <Router>
+          <div>
+            <Navbar bg="light" variant="light">
+              <Container>
+                <Nav className="me-auto">
+                  {currentPage === '/' ? (
+                    <Nav.Link href="/login">Log in</Nav.Link>
+                  ) : currentPage === '/login' ? (
+                    <Nav.Link href="/">Regresar</Nav.Link>
+                  ) : (
+                    <Nav.Link href="/">Log out</Nav.Link>
+                  )}
+                </Nav>
+              </Container>
+            </Navbar>
 
-        <div className="container">
-          <Routes>
-          <Route path= "/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/inicio" element={<Inicio />} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Routes>
-        </div>
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/inicio" element={<Inicio />} />
+                <Route path="*" element={<h1>404 Not Found</h1>} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
       </div>
-      </Router>
-    </div>
+    </CookiesProvider>
   );
 }
 
